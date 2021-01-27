c44ece6b3d859c44881503948cf2257a80b833f4
anilsahu17 <anil.constacloud@gmail.com>
2021-01-27 12:49:28 +0530

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './Routers/app';
+import './Assets/css/app.css';
+ReactDOM.render(<AppRouter/>, document.getElementById('App')
+);
+
+
